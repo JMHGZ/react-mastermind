3a186d7a2e194ae498c0aef40e562624ab50275f
Jeremiah Garza <jag_tt@yahoo.com>
2019-09-26 21:17:29 -0500

    compnents add

---
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./ColorPicker.module.css";
+
+const ColorPicker = props => (
+  <div>
+    {props.colors.map((color, idx) => (
+      <button
+        className={styles.button}
+        style={{
+          backgroundColor: idx === props.selColorIdx ? "white" : color,
+          borderColor: color
+        }}
+        key={color}
+      ></button>
+    ))}
+  </div>
+);
+
+export default ColorPicker;
---
+++ src/components/NewGameButton/NewGameButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewGameButton = props => (
+  <button className="btn btn-default">New Game</button>
+);
+
+export default NewGameButton;
